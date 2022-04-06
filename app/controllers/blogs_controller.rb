f810b3369a4bfa7a0790a00b6d2d3b027d89e284
EC2 Default User <ec2-user@ip-172-31-21-94.ap-northeast-1.compute.internal>
2022-04-06 12:35:16 +0000

    削除機能の実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,9 +12,19 @@ class BlogsController < ApplicationController
         Blog.create(blog_params)
     end 
     
+    def destroy
+        blog = Blog.find(params[:id])
+        blog.destroy
+        redirect_to action: :index
+        
+    end
+    
     private
     def blog_params
         params.require(:blog).permit(:text)
     end 
     
+    def move_to_index
+        redirect_to action: :index
+    end 
 end
