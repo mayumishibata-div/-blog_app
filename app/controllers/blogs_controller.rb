0e8a837be4c66f9a0781edd322d51c9a548c86ac
EC2 Default User <ec2-user@ip-172-31-21-94.ap-northeast-1.compute.internal>
2022-04-06 09:45:06 +0000

    ブログ投稿ページの実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,10 @@
 class BlogsController < ApplicationController
     
     def index
-        @blogs =Blog.all
+        @blogs = Blog.all
+    end 
+    
+    def new
+        @blog = Blog.new
     end 
 end
